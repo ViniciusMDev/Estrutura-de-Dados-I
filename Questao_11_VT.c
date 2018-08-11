6f797503f6e5ba59d2533d8861dcf9444a33acb7
Vinicius Martins <39231728+ViniciusMDev@users.noreply.github.com>
2018-08-11 02:44:11 -0300

    Create Questao_11_VT.c

---
+++ Questao_11_VT.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct tContas{     //criando minha struct
+    int codigo;
+    char cliente[15];
+    float saldo;
+
+
+}Contas;
+
+int main()
+{
+    Contas vetor[2];
+    int i ,codigodeposito ,codigosaque;
+    float valordeposito, valorsaque;
+    char copianome[15];
+    for(i=0;i<2;i++){                                   //preenchimento do vetor de struct
+        printf("\nDigite o codigo da conta: ");
+        scanf("%d", &vetor[i].codigo);
+        getchar();
+        printf("\nDigite o seu nome: ");
+        gets(vetor[i].cliente);
+        printf("\nDigite o seu saldo: ");
+        scanf("%f", &vetor[i].saldo);
+        printf("==========================");
+
+
+    }
+    int opcao;
+    do{
+        printf("\nDigite o codigo da operacao: 1 - Deposito, 2 - Saque, 0 - Sair\n"); //selecao de operação a ser realizada
+        scanf("%d", &opcao);
+
+    }
+    while (opcao != 1 && opcao != 2 && opcao != 0);
+
+    strcpy(copianome,vetor[0].cliente);
+    if (opcao == 1){
+        printf("\nDigite o codigo da conta: ");            //opção de depósito
+        scanf("%d", &codigodeposito);
+        printf("\nDigite o valor a ser depositado: ");
+        scanf("%f", &valordeposito);
+        for(i=0;i<5;i++){
+            if(vetor[i].codigo == codigodeposito){
+                vetor[i].saldo = vetor[i].saldo + valordeposito;
+                strcpy(copianome,vetor[i].cliente);                    //impressão do saldo e nome atualizado
+                printf("==========================");
+                printf("\nSaldo atualizado: %.2f", vetor[i].saldo);
+                printf("\nObrigado pela operacao %s \n\n", copianome);
+            }
+        }
+    }
+    if (opcao == 2){
+        printf("\nDigite o codigo da conta: ");            //opção de saque
+        scanf("%d", &codigosaque);
+        printf("\nDigite o valor a ser sacado: ");
+        scanf("%f", &valorsaque);
+        for(i=0;i<5;i++){
+            if(vetor[i].codigo == codigosaque){
+                if(vetor[i].saldo>=valorsaque){
+                vetor[i].saldo = vetor[i].saldo - valorsaque;
+                strcpy(copianome,vetor[i].cliente);                    //impressão do saldo e nome atualizado
+                printf("==========================");
+                printf("\nSaldo atualizado: %.2f", vetor[i].saldo);
+                printf("\nObrigado pela operacao %s \n\n", copianome);
+                }
+                else{
+                    printf("Saldo Insuficiente");
+                }
+            }
+        }
+
+    }
+
+    return 0;
+}
