5e4b06be425d929d6ff17c4cf891b2092cf4493a
Vinicius Martins <39231728+ViniciusMDev@users.noreply.github.com>
2018-08-11 02:32:00 -0300

    Create Questao_6_VT.c

---
+++ Questao_6_VT.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int vetor1[5], vetor2[8],i,j;
+    for(j=0;j<5;j++){
+        printf("Digite o elemento %d do vetor A: ", j+1);       //preenchimento do vetor A
+        scanf("%d", &vetor1[j]);
+    }
+    for(i=0;i<8;i++){
+        printf("Digite o elemento %d do vetor B: ", i+1);  //preenchimento do vetor B
+        scanf("%d", &vetor2[i]);
+    }
+    for(i=0;i<8;i++){
+        for(j=0;j<5;j++){                                       //verificação e impressão dos elementos comuns
+        if(vetor2[i]==vetor1[j]){
+            printf("Elemento comum : %d\n",vetor2[i]);
+        }
+        }
+
+        }
+
+
+    return 0;
+}
