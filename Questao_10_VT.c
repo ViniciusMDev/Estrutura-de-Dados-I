314315c98ca0b1397e4e5a022d80384586c30988
Vinicius Martins <39231728+ViniciusMDev@users.noreply.github.com>
2018-08-11 02:53:38 -0300

    Update Questao_10_VT.c

--- Questao_10_VT.c
+++ Questao_10_VT.c
@@ -1,3 +1,11 @@
+/*10. Crie uma estrutura representando os alunos de um determinado curso. A estrutura deve conter a
+matrícula do aluno, nome, nota da primeira prova, nota da segunda prova e nota da terceira prova.
+Permita ao usuário entrar com os dados de 5 alunos.
+Encontre o aluno com maior nota da primeira prova.
+Encontre o aluno com maior media geral.
+Encontre o aluno com menor media geral.
+Para cada aluno diga se ele foi aprovado ou reprovado, considerando o valor 6 para aprovação.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
