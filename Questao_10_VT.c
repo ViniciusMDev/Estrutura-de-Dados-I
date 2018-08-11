c22315fbb07afbe04d464a3463a3ed414d19fd11
Vinicius Martins <39231728+ViniciusMDev@users.noreply.github.com>
2018-08-11 02:43:22 -0300

    Create Questao_10_VT.c

---
+++ Questao_10_VT.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct tNotasAlunos{            //criação da struct
+    char nome[50];
+    float nota1, nota2, nota3;
+    int matricula;
+
+}NotasAlunos;
+
+int main()
+{
+    NotasAlunos vetor[5];
+    int i;
+    char alunomenor[50], alunomaior[50], alunomaiorprimeira[50];
+    float maiorp1, maiorgeral, menorgeral, media;
+
+    for(i=0;i<5;i++){
+        printf("\nDigite a matricula do aluno: ");
+        scanf("%d", &vetor[i].matricula);
+        getchar();
+        printf("\nDigite o nome do aluno: ");                       //preenchimento do vetor de struct
+        gets(vetor[i].nome);
+        printf("\nDigite a primeira nota do aluno: ");
+        scanf("%f", &vetor[i].nota1);
+        printf("\nDigite a segunda nota do aluno: ");
+        scanf("%f", &vetor[i].nota2);
+        printf("\nDigite a terceira nota do aluno: ");
+        scanf("%f", &vetor[i].nota3);
+        printf("====================================\n");
+    }
+    maiorp1= vetor[0].nota1;
+    maiorgeral=0;
+    menorgeral = 999;                         //atribuindo valor as variáveis para não pegar lixo de memória
+    media=0;
+
+    strcpy(alunomaiorprimeira,vetor[0].nome);
+    strcpy(alunomaior,vetor[0].nome);
+    strcpy(alunomenor,vetor[0].nome);
+
+    for(i=0;i<5;i++){
+        media = (vetor[i].nota1 + vetor[i].nota2 + vetor[i].nota3) / 3;
+        if(vetor[i].nota1>maiorp1){                                                 //calculo da média e verificação do aluno com a maior primeira nota
+            maiorp1 = vetor[i].nota1;
+            strcpy(alunomaiorprimeira,vetor[i].nome);
+        }
+        if(media>maiorgeral){
+            maiorgeral = media;                   //buscando aluno com maior media geral
+            strcpy(alunomaior,vetor[i].nome);
+        }
+        if(media<menorgeral){                     //buscando aluno com menor media geral
+            menorgeral = media;
+            strcpy(alunomenor,vetor[i].nome);
+        }
+        if(media>=6){
+            printf("Aluno %s Aprovado\n\n", vetor[i].nome);
+        }                                                                   //imprimindo se o aluno foi aprovado ou não
+        else{
+            printf("Aluno %s Reprovado\n\n", vetor[i].nome);
+        }
+    media=0;
+    }
+
+    printf("O aluno com a maior nota da primeira prova foi %s com %.2f\n\n", alunomaiorprimeira, maiorp1);  //imprimindo o que foi pedido
+    printf("O aluno com a maior media geral foi %s com %.2f\n\n", alunomaior, maiorgeral);
+    printf("O aluno com a menor media geral foi %s com %.2f\n\n", alunomenor, menorgeral);
+
+
+    return 0;
+}
